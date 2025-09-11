89362e7a5867e19e7a6f0b2b988ef70e31ce11b6
Felipe Brandalise <fbrandalise@gmail.com>
2025-09-11 11:35:03 -0300

    teste

--- src/pages/Analysis.tsx
+++ src/pages/Analysis.tsx
@@ -373,10 +373,10 @@ const Analysis = () => {
                   <div 
                     className="absolute top-5 left-16 right-5 bottom-10 pointer-events-none"
                     style={{
-                      background: 'rgba(199, 253, 5)',
+                /*      background: 'rgba(199, 253, 5)',
                       position: 'absolute; inset: 0',
                       pointer-events: 'none',
-                      clip-path: 'polygon(0% 100%, 50% 100%, 100% 0%)',
+                      clip-path: 'polygon(0% 100%, 50% 100%, 100% 0%)',*/
                     }}
                   />
                   
