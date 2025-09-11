913b0b69bc87af6fc4a1cdbb4a3705e7d77f5a9e
Felipe Brandalise <fbrandalise@gmail.com>
2025-09-11 07:39:13 -0300

    alterando form do poligono amarelo

--- src/pages/Analysis.tsx
+++ src/pages/Analysis.tsx
@@ -374,7 +374,7 @@ const Analysis = () => {
                     className="absolute top-5 left-16 right-5 bottom-10 pointer-events-none"
                     style={{
                       background: 'rgba(199, 253, 5)',
-                      clipPath: 'polygon(0% 25%, 50% 0%, 100% 0%, 100% 100%)'
+                      clipPath: 'polygon(0% 50%, 50% 0%, 100% 0%, 100% 100%)'
                     }}
                   />
                   
