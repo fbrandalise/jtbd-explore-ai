b3e1d2d6e57f30b9fb710a85aa6de3d8072be22a
Felipe Brandalise <fbrandalise@gmail.com>
2025-09-11 07:49:35 -0300

    alterando poligono amarelo

--- src/pages/Analysis.tsx
+++ src/pages/Analysis.tsx
@@ -374,7 +374,7 @@ const Analysis = () => {
                     className="absolute top-5 left-16 right-5 bottom-10 pointer-events-none"
                     style={{
                       background: 'rgba(199, 253, 5)',
-                      clipPath: 'polygon(0% 0%, 100% 0%, 0% 100%)'
+                      clipPath: 'polygon(0% 100%, 0% 100%, 0% 100%)'
                     }}
                   />
                   
