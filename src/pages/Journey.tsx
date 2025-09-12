89e51850fee41900f1d4210e1676fc2e98ca602b
Felipe Brandalise <fbrandalise@gmail.com>
2025-09-12 10:19:02 -0300

    drag off agora vai

--- src/pages/Journey.tsx
+++ src/pages/Journey.tsx
@@ -158,8 +158,8 @@ const Journey = () => {
         type: 'bigJob',
         //position: { x: 0, y: bigJobY },
         position: { x: bigJobX, y: bigJobY },
+        dragging: false,
         data: {
-          dragging: false,
           label: bigJob.name,
           description: bigJob.description,
           count: bigJob.littleJobs.length,
